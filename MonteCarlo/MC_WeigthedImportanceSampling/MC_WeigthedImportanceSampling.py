61082dca4525575c7e75e529419fa54ac892f1a0
simon555 <simon.sebbagh@free.fr>
2017-12-31 22:35:41 -0500

    adding weigthed importance sampling

--- MonteCarlo/MC_WeigthedImportanceSampling/MC_WeigthedImportanceSampling.py
+++ MonteCarlo/MC_WeigthedImportanceSampling/MC_WeigthedImportanceSampling.py
@@ -7,12 +7,11 @@ import sys
 from collections import defaultdict
 
 from envs.blackjack import BlackjackEnv
-from lib import plotting
+from utils import plotting
 
 
 
 env = BlackjackEnv()
-
 
 
 def create_random_policy(nA):
@@ -44,11 +43,12 @@ def create_greedy_policy(Q):
         of action probabilities.
     """
     
-    def policy_fn(observation):
-        pass
-        # Implement this!
+    def policy_fn(state):
+        A = np.zeros_like(Q[state], dtype=float)
+        best_action = np.argmax(Q[state])
+        A[best_action] = 1.0
+        return A
     return policy_fn
-
 
 
 def mc_control_importance_sampling(env, num_episodes, behavior_policy, discount_factor=1.0):
@@ -73,11 +73,52 @@ def mc_control_importance_sampling(env, num_episodes, behavior_policy, discount_
     # The final action-value function.
     # A dictionary that maps state -> action values
     Q = defaultdict(lambda: np.zeros(env.action_space.n))
+    # The cumulative denominator of the weighted importance sampling formula
+    # (across all episodes)
+    C = defaultdict(lambda: np.zeros(env.action_space.n))
     
     # Our greedily policy we want to learn
     target_policy = create_greedy_policy(Q)
-    
-    # Implement this!
+
+    for i_episode in range(1, num_episodes + 1):
+        # Print out which episode we're on, useful for debugging.
+        if i_episode % 1000 == 0:
+            print("\rEpisode {}/{}.".format(i_episode, num_episodes), end="")
+            sys.stdout.flush()
+
+        # Generate an episode.
+        # An episode is an array of (state, action, reward) tuples
+        episode = []
+        state = env.reset()
+        for t in range(100):
+            # Sample an action from our policy
+            probs = behavior_policy(state)
+            action = np.random.choice(np.arange(len(probs)), p=probs)
+            next_state, reward, done, _ = env.step(action)
+            episode.append((state, action, reward))
+            if done:
+                break
+            state = next_state
+        
+        # Sum of discounted returns
+        G = 0.0
+        # The importance sampling ratio (the weights of the returns)
+        W = 1.0
+        # For each step in the episode, backwards
+        for t in range(len(episode))[::-1]:
+            state, action, reward = episode[t]
+            # Update the total reward since step t
+            G = discount_factor * G + reward
+            # Update weighted importance sampling formula denominator
+            C[state][action] += W
+            # Update the action-value function using the incremental update formula (5.7)
+            # This also improves our target policy which holds a reference to Q
+            Q[state][action] += (W / C[state][action]) * (G - Q[state][action])
+            # If the action taken by the behavior policy is not the action 
+            # taken by the target policy the probability will be 0 and we can break
+            if action !=  np.argmax(target_policy(state)):
+                break
+            W = W * 1./behavior_policy(state)[action]
         
     return Q, target_policy
 
